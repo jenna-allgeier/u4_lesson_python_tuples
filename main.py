b9dcd654fdc940f7c2032076b3d4ffab190f9d46
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-05-02 09:26:30 -0400

    all tests passing

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 def create_tuple(list_of_nums):
     # Create a tuple from a list and return it.
-    return
+    my_tup = tuple(list_of_nums)
+    return my_tup
 
 
 def get_value(tup):
     # Return the 3rd value in the provided tuple
-    return
+    return tup[2]
 
 
 def get_values(tup):
     # return the values from index 1-3
     # 3 should be inclusive
-    return
+    return tup[1:4]
 
 
 def get_max(tup):
     # return the largest number in the provided tuple
-    return
+    return max(tup)
 
